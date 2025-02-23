8a4b801538d82f18726db8d1ad46c5e2f1bbfc4b
shiran vatury <shiran.vatury@votiro.com>
2025-02-23 13:09:13 +0200

    small change

--- pipeline/app.py
+++ pipeline/app.py
@@ -3,14 +3,28 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/catfact', methods=['GET'])
+health_status = True
+
+@app.route('/', methods=['GET'])
 def get_cat_fact():
     response = requests.get('https://catfact.ninja/fact')
     if response.status_code == 200:
-        fact = response.json().get(response.content)
+        fact = response.json().get('fact', 'No fact available')
         return jsonify({'cat_fact': fact})
     else:
         return jsonify({'error': 'Failed to fetch cat fact'}), response.status_code
 
+@app.route('/health')
+def health():
+    if health_status:
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
+
